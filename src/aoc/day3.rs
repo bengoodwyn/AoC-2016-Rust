2195e24de51d83a66385ad365800770e0cda0cf5
Ben Goodwyn <ben@clouddirective.com>
2017-11-14 07:39:32 -0600

    Day 3 Example 1

--- src/aoc/day3.rs
+++ src/aoc/day3.rs
@@ -2,9 +2,20 @@
 mod part1_tests {
     #[test]
     fn it_passes_example1() {
+        assert_eq!(0, super::part1("5 10 25"))
     }
 }
 
-pub fn part1(input: &str) -> i32 {
-    0
+pub fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line|
+            line
+                .split_whitespace()
+                .map(|value| value.parse::<u32>().unwrap() )
+                .collect::<Vec<u32>>()
+        )
+        .filter(|vec| vec.len() == 3)
+        .filter(|vec| (vec[0] + vec[1] > vec[2]))
+        .count()
 }
