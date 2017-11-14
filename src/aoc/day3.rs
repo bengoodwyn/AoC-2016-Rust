8b69895d07e0289fc45be4cce13a70a4b00d4772
Ben Goodwyn <ben@clouddirective.com>
2017-11-14 07:15:52 -0600

    Day 3 stub

---
+++ src/aoc/day3.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod part1_tests {
+    #[test]
+    fn it_passes_example1() {
+    }
+}
+
+pub fn part1(input: &str) -> i32 {
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     println!("Day 1 Part 2: {}", aoc::day1::part2(&input::read("inputs/1.txt")));
     println!("Day 2 Part 1: {}", aoc::day2::part1(&input::read("inputs/2.txt")));
     println!("Day 2 Part 2: {}", aoc::day2::part2(&input::read("inputs/2.txt")));
+    println!("Day 3 Part 1: {}", aoc::day3::part1(&input::read("inputs/3.txt")));
 }
