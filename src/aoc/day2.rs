e9b71896cd6b829468e8f5961fcde8b0a2de2b10
Ben Goodwyn <ben@clouddirective.com>
2017-11-13 19:38:58 -0600

    Working with different types instead of nested matches for keypad

--- src/aoc/day2.rs
+++ src/aoc/day2.rs
@@ -1,115 +1,83 @@
 use aoc::input;
 
-fn new_key(start_key: char, direction: char) -> char {
-    match direction {
-        'U' => match start_key {
-            '4' => '1',
-            '5' => '2',
-            '6' => '3',
-            '7' => '4',
-            '8' => '5',
-            '9' => '6',
-            _ => start_key
-        },
-        'D' => match start_key {
-            '1' => '4',
-            '2' => '5',
-            '3' => '6',
-            '4' => '7',
-            '5' => '8',
-            '6' => '9',
-            _ => start_key
-        },
-        'L' => match start_key {
-            '2' => '1',
-            '3' => '2',
-            '5' => '4',
-            '6' => '5',
-            '8' => '7',
-            '9' => '8',
-            _ => start_key
-        },
-        'R' => match start_key {
-            '1' => '2',
-            '2' => '3',
-            '4' => '5',
-            '5' => '6',
-            '7' => '8',
-            '8' => '9',
-            _ => start_key
-        },
-        _ => panic!("Invalid direction {}", direction)
+#[derive(Debug, Clone, Copy)]
+struct Position {
+    x: usize,
+    y: usize
+}
+
+const KEYPAD_DIMENSION: usize = 5;
+
+struct Keypad {
+    initial_position: Position,
+    keys: [[char; KEYPAD_DIMENSION]; KEYPAD_DIMENSION]
+}
+
+const BASIC_KEYPAD : Keypad = Keypad{
+    initial_position: Position{x:2,y:2},
+    keys:
+[
+[ ' ', ' ', ' ' ,' ', ' ' ],
+[ ' ', '1', '2', '3', ' ' ],
+[ ' ', '4', '5', '6', ' ' ],
+[ ' ', '7', '8', '9', ' ' ],
+[ ' ', ' ', ' ' ,' ', ' ' ]
+]
+};
+
+const CRAZY_KEYPAD : Keypad = Keypad{
+    initial_position: Position{x:0,y:2},
+    keys:
+[
+[ ' ', ' ', '1' ,' ', ' ' ],
+[ ' ', '2', '3' ,'4', ' ' ],
+[ '5', '6', '7' ,'8', '9' ],
+[ ' ', 'A', 'B' ,'C', ' ' ],
+[ ' ', ' ', 'D' ,' ', ' ' ]
+]
+};
+
+impl Position {
+    fn moved(&self, direction: char) -> Position {
+        match direction {
+            'U' if self.y > 0 => Position{ x: self.x, y: self.y-1 },
+            'D' if self.y < (KEYPAD_DIMENSION-1) => Position{ x: self.x, y: self.y+1 },
+            'L' if self.x > 0 => Position{ x: self.x-1, y: self.y },
+            'R' if self.x < (KEYPAD_DIMENSION-1) => Position{ x: self.x+1, y: self.y },
+            _ => Position{ x: self.x, y: self.y }
+        }
     }
 }
 
-fn new_crazy_key(start_key: char, direction: char) -> char {
-    match direction {
-        'U' => match start_key {
-            '3' => '1',
-            '6' => '2',
-            '7' => '3',
-            '8' => '4',
-            'A' => '6',
-            'B' => '7',
-            'C' => '8',
-            'D' => 'B',
-            _ => start_key
-        },
-        'D' => match start_key {
-            '1' => '3',
-            '2' => '6',
-            '3' => '7',
-            '4' => '8',
-            '6' => 'A',
-            '7' => 'B',
-            '8' => 'C',
-            'B' => 'D',
-            _ => start_key
-        },
-        'L' => match start_key {
-            '9' => '8',
-            '4' => '3',
-            '8' => '7',
-            'C' => 'B',
-            '3' => '2',
-            '7' => '6',
-            'B' => 'A',
-            '6' => '5',
-            _ => start_key
-        },
-        'R' => match start_key {
-            '8' => '9',
-            '3' => '4',
-            '7' => '8',
-            'B' => 'C',
-            '2' => '3',
-            '6' => '7',
-            'A' => 'B',
-            '5' => '6',
-            _ => start_key
-        },
-        _ => panic!("Invalid direction {}", direction)
+fn new_position(start_pos: Position, direction: char, keypad: &Keypad) -> Position {
+    let new_pos = start_pos.moved(direction);
+    if keypad.keys[new_pos.y][new_pos.x] == ' ' {
+        start_pos
+    } else {
+        new_pos
     }
 }
 
-fn keypad(filename: &str, keypad_fn: &Fn(char, char) -> char) -> String {
+fn keypad(filename: &str, keypad: &Keypad) -> String {
     let input = input::read(&filename);
     input
         .lines()
-        .map(|line|{
-            line
+        .map(|line| {
+            let Position{ x, y } =
+                line
                 .chars()
-                .fold('5', |key, direction| {
-                    keypad_fn(key, direction)
-                })
+                .fold(keypad.initial_position, |position, direction| {
+                    new_position(position, direction, keypad)
+                });
+            keypad.keys[x][y]
         })
         .collect::<String>()
 }
 
 pub fn part1(filename: &str) -> String {
-    keypad(filename, &new_key)
+    keypad(filename, &BASIC_KEYPAD)
 }
 
 pub fn part2(filename: &str) -> String {
-    keypad(filename, &new_crazy_key)
+    keypad(filename, &CRAZY_KEYPAD)
 }
