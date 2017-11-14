b3b9e94e7812d01fa84798dfbe66e1dbf5b5f80e
Ben Goodwyn <ben@clouddirective.com>
2017-11-14 07:43:50 -0600

    Day 3 Part 1 More Tests Forcing Sort

--- src/aoc/day3.rs
+++ src/aoc/day3.rs
@@ -2,7 +2,17 @@
 mod part1_tests {
     #[test]
     fn it_passes_example1() {
-        assert_eq!(0, super::part1("5 10 25"))
+        assert_eq!(0, super::part1("5 10 25"));
+    }
+
+    #[test]
+    fn it_passes_an_invalid_triangle_in_reverse_order() {
+        assert_eq!(0, super::part1( "25 10 5"));
+    }
+
+    #[test]
+    fn it_passes_a_valid_triangle() {
+        assert_eq!(1, super::part1( "3 4 5"));
     }
 }
 
@@ -15,6 +25,11 @@ pub fn part1(input: &str) -> usize {
                 .map(|value| value.parse::<u32>().unwrap() )
                 .collect::<Vec<u32>>()
         )
+        .map(|unsorted| {
+            let mut sorted = unsorted.clone();
+            sorted.sort();
+            sorted
+        })
         .filter(|vec| vec.len() == 3)
         .filter(|vec| (vec[0] + vec[1] > vec[2]))
         .count()
